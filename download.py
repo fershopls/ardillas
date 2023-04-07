7ef72ddb0d6a8bcb111b0b52e97b24c5b6b04c2c
Fernando <shaaf4ever@gmail.com>
2023-04-07 08:12:58 -0600

    Two files

--- download.py
+++ download.py
@@ -22,9 +22,15 @@ def main(url):
     streams = video.streams.filter(only_audio=True)
     streams.first().download(filename=mp4_file)
 
-    cmd = f'ffmpeg -y -i {mp3_name}.mp4 -af asetrate=44100*1.75,aresample=44100,atempo=1/1.75 -c:a libmp3lame "static/downloads/{mp3_name}.mp3"'
+    cmd = f'ffmpeg -y -i {mp3_name}.mp4 -af asetrate=44100*1.75,aresample=44100,atempo=1/1.75 -c:a libmp3lame "static/downloads/first-{mp3_name}.mp3"'
     print(cmd)
     os.system(cmd)
+    
+    cmd = f'ffmpeg -y -i "static/downloads/first-{mp3_name}.mp3" -c:a libmp3lame "static/downloads/{mp3_name}.mp3"'
+    print(cmd)
+    os.system(cmd)
+
+    os.remove(f"static/downloads/first-{mp3_name}.mp3")
     os.remove(mp4_file)
 
 main(args.url)
